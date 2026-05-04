a050f9ad302f182b869406cf452b38fc4f0bf7f4
Angelie <angelie.angman@gmail.com>
2026-05-04 19:13:15 +0200

    update

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
-const btn = document.querySelector(".btn");
-const animation = document.querySelector(".animation");
-const svg = document.querySelector(".svg");
+const firstBtn = document.querySelector(".btn");
+const secondBtn = document.querySelector(".second-btn");
+const thirdBtn = document.querySelector(".third-btn");
+const fourthBtn = document.querySelector(".fourth-btn");
+const spritesheet = document.querySelector(".cat--sprite");
+const drawing = document.querySelector(".cat--vector");
+const text = document.querySelector(".text--vector");
 
-btn.addEventListener("click", () => {
-  animation.classList.add("hidden");
-  svg.classList.remove("hidden");
+firstBtn.addEventListener("click", () => {
+  spritesheet.classList.remove("hidden");
+  text.classList.add("hidden");
+});
+
+secondBtn.addEventListener("click", () => {
+  drawing.classList.remove("hidden");
+  text.classList.add("hidden");
+});
+
+thirdBtn.addEventListener("click", () => {
+  text.classList.remove("hidden");
+  drawing.classList.add("hidden");
+});
+
+fourthBtn.addEventListener("click", () => {
+  spritesheet.classList.add("hidden");
+  text.classList.remove("hidden");
+});
+
+gsap.to(".text--vector path", {
+  x: 0,
+  opacity: 1,
+  stagger: 0.2,
+});
+
+gsap.to("#e", {
+  scale: 0.5,
+  rotation: 360,
+  duration: 4,
+  transformOrigin: "center center",
+  repeat: -1,
+  yoyo: true,
+});
+gsap.to("#o", {
+  scale: 0.5,
+  transformOrigin: "left center",
+  duration: 5,
+  repeat: -1,
+  yoyo: true,
+});
+
+gsap.to("#cat", {
+  stroke: "#ff00ff",
+  duration: 1,
+  repeat: -1,
+  yoyo: true,
 });
